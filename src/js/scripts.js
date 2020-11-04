7b350dd4295df6b48777b6967a61a55d5e53299b
Maxence Fernandez <pro.fndz@gmail.com>
2020-11-05 00:37:50 +0100

    [FEAT] Finish search and data presentation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,4 @@
-var exampleData = '{"global":10,"region":12,"regionName":"MaRégion","departement":50,"departementName":"MonDep","digitalInterfaceAccess":123,"informationAccess":456,"administrativeCompetences":789,"digitalAndScolarCompetences":42}';
+var exampleData = '{"global":10,"region":62,"regionName":"Occitanie","departement":50,"departementName":"Hérault","digitalInterfaceAccess":123,"informationAccess":456,"administrativeCompetences":789,"digitalAndScolarCompetences":42}';
 
 function search() {
   let value = document.getElementById("searchbar").value;
@@ -8,17 +8,42 @@ function search() {
   showResult(JSON.parse(exampleData));
 }
 
+function color(first, second, value) {
+  let color = 'red';
+    if (value > second)
+      color = 'lightgreen';
+    else if (value > first)
+      color = 'yellow';
+    return color
+}
+
 
 function showResult(jsonDump) {
+
+
   document.getElementById("global-score").innerText = jsonDump["global"];
+  document.getElementById("global-score-indicator").style.backgroundColor = color(138,220, jsonDump["global"]);
+
   document.getElementById("region-name").innerText = jsonDump["regionName"];
   document.getElementById("region-score").innerText = jsonDump["region"];
+  document.getElementById("region-score-indicator").style.backgroundColor = color(0,0, jsonDump["region"]);
+
   document.getElementById("departement-name").innerText = jsonDump["departementName"];
   document.getElementById("departement-score").innerText = jsonDump["departement"];
+  document.getElementById("departement-score-indicator").style.backgroundColor = color(0,0, jsonDump["departement"]);
+
   document.getElementById("digitalInterfaceAccess-score").innerText = jsonDump["digitalInterfaceAccess"];
+  document.getElementById("digitalInterfaceAccess-score-indicator").style.backgroundColor = color(108,173, jsonDump["digitalInterfaceAccess"]);
+
   document.getElementById("informationAccess-score").innerText = jsonDump["informationAccess"];
+  document.getElementById("informationAccess-score-indicator").style.backgroundColor = color(252,404, jsonDump["informationAccess"]);
+
   document.getElementById("administrativeCompetences-score").innerText = jsonDump["administrativeCompetences"];
+  document.getElementById("administrativeCompetences-score-indicator").style.backgroundColor = color(298,477, jsonDump["administrativeCompetences"]);
+
   document.getElementById("digitalAndScolarCompetences-score").innerText = jsonDump["digitalAndScolarCompetences"];
+  document.getElementById("digitalAndScolarCompetences-score-indicator").style.backgroundColor = color(215,344, jsonDump["digitalAndScolarCompetences"]);
+
   document.getElementById("result-div").style.display = "inline-block";
 
 }
@@ -31,14 +56,7 @@ function governmentApiCallback(jsonDump) {
   console.log(jsonDump);
 }
 
-function keyPressed() {
-  if (!e) e = window.event;
-  var keyCode = e.keyCode || e.which;
-  if (keyCode === '13'){
-    // Enter pressed
-    console.log("ENTER")
-  }
-}
+
 
 
 function getUrl(url, callback, errorMessage) {
@@ -48,7 +66,6 @@ function getUrl(url, callback, errorMessage) {
       if (xmlHttp.status === 200) {
         callback(JSON.parse(xmlHttp.responseText));
       } else {
-        console.log("ok");
         alert(errorMessage)
       }
     }
